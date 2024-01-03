6cabe6d65da19a81679647165cb5ed7f38c34d66
pakapaka <2ulkiki0621@gmail.con>
2024-01-03 12:50:28 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+const home = require("./routes/home");
+//앱세팅
+app.set("views","./views");
+app.set("view engine","ejs");
+
+
+app.use("/",home);// use ->미들웨어를 등록해주는 메서드 
+
+
+module.exports =app;
